f3f21aaa0af974f5d04d11d16ee5dbdd33487100
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-23 18:52:26 +0300

    style: add basic styles

---
+++ src/modules/TaskCanvas/components/TaskInput/TaskInput.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="task_text-area">
+        <textarea maxlength="200" placeholder="Text of task"></textarea>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .task_text-area {
+        height: 35px;
+        padding-top: 5px;
+        width: 100%;
+        background-color: #fff;
+        border-radius: 5px;
+        overflow: hidden;
+        
+        textarea {
+            display: inline-block;
+            overflow: hidden;
+            width: 100%;
+            height: 100%;
+            resize: none;
+            padding: 5px 15px;
+            font-size: 14px;
+            &:focus {
+                outline: none;
+            }
+    }
+    }
+    
+</style>

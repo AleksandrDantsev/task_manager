810effc07b51c1c37be7fb9150104235942ecfac
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-04-23 19:42:49 +0300

    feat: add icon svg

--- src/modules/TaskCanvas/components/TaskInput/TaskInput.vue
+++ src/modules/TaskCanvas/components/TaskInput/TaskInput.vue
@@ -1,10 +1,26 @@
 <script setup>
-
+import typeIcon from "../../../../assets/svg/typeTask.svg";
+import assigneesIcon from "../../../../assets/svg/assignees.svg";
+import projectIcon from "../../../../assets/svg/project.svg"
 </script>
 
 <template>
-    <div class="task_text-area">
-        <textarea maxlength="200" placeholder="Text of task"></textarea>
+    <div class="task_input-block">
+        <div class="task_text-area">
+            <textarea maxlength="200" placeholder="Text of task"></textarea>
+        </div>
+        <div class="task_input-info">
+            <div class="task_input__maxlength">200</div>
+            <div class="task_input__hint">Press Enter</div>
+            <div class="task_input__info-task">
+                <ul>
+                    <li class="task_type"><img :src="typeIcon" alt="type"></li>
+                    <li class="task_assignees"><img :src="assigneesIcon" alt="assignees"></li>
+                    <li class="task_project"><img :src="projectIcon" alt="project"></li>
+                </ul>
+                
+            </div>
+        </div>
     </div>
 </template>
 
@@ -29,6 +45,9 @@
                 outline: none;
             }
     }
+    }
+    .task_input-info {
+        background-color: #fff;
     }
     
 </style>

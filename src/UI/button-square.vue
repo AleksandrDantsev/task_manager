7ccc7c7f26f0a6be5c331cd8536d4ca7cf60bd12
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-01 13:07:27 +0300

    chore: create ui button

---
+++ src/UI/button-square.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+    content: String | Number,
+})
+</script>
+
+<template>
+    <button class="button">
+        {{ props.content }}
+    </button>
+</template>
+
+<style scoped lang="scss">
+    .button {
+        height: 24px;
+        width: 24px;
+        border-radius: 3px;
+        font-size: 12px;
+        color: #8d8d8d;
+        font-weight: 600;
+        background-color: #f5f5f5d8;
+    }
+</style>

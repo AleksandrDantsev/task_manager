0c812f43f8df94dc058dd92ccb8d718d2bdac4f8
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-01 13:05:14 +0300

    feat: create date component

---
+++ src/modules/TaskCanvas/components/TaskDate/TaskDate.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+    date: String,
+})
+
+
+</script>
+
+<template>
+    <div class="task_date">
+        {{ date }}
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .task_date {
+        color: #a8a8a8;
+        padding: 10px 0;
+    }
+</style>

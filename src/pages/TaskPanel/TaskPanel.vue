dc67a3bf065593b6bf63dba8f3c2ffdb3669e311
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-01 13:06:31 +0300

    feat: add field for tasks

---
+++ src/pages/TaskPanel/TaskPanel.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { computed, ref } from 'vue';
+import TaskCanvas from '../../modules/TaskCanvas/main/TaskCanvas.vue';
+import { getReadableDate } from "../../helpers/getReadableDate";
+
+const arrayDateShift = computed(() => {
+    const arrayDate = [];
+    for (let i = 0; i < 7; i++) {
+        arrayDate.push(getReadableDate(-i))
+    }
+    return arrayDate;
+})
+
+</script>
+
+<template>
+    <div class="task-panel">
+        <TaskCanvas v-for="item of arrayDateShift"
+                    :key="item"
+                    :date="item"/>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .task-panel {
+        display: flex;
+        width: 100%; 
+        min-height: 100%;
+        overflow-x: auto;
+        background-color: #f5f5f5d8;
+    }
+</style>

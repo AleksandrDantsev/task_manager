2d4cfd56019bed5a060eb29d0203a6e44a905169
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-06 15:01:22 +0300

    get data form the stor for cards

--- src/modules/TaskCanvas/main/TaskCanvas.vue
+++ src/modules/TaskCanvas/main/TaskCanvas.vue
@@ -2,17 +2,17 @@
 import TaskDate from '../components/TaskDate/TaskDate.vue';
 import TaskInput from '../components/TaskInput/TaskInput.vue';
 import TaskCard from '../components/TaskCard/TaskCard.vue';
-import { LS } from '../../../helpers/LocalStorageAction';
-import { computed, ref, reactive, onMounted } from 'vue';
+import { ref, computed } from 'vue';
+import { useStore } from "vuex";
 
 const props = defineProps({
     date: String,
 });
 
 const nameTask = ref('');
-const cards = ref(LS.getTasks(props.date));
-
-const changeState = () => cards.value = LS.getTasks(props.date);
+const store = useStore();
+const cards = computed(() => store.state.tasksLocalArray?.[props.date]);
+console.log(cards)
 
 </script>
 
@@ -20,18 +20,16 @@ const changeState = () => cards.value = LS.getTasks(props.date);
     <div class="task-canvas">
         <TaskDate :date="props.date"/>
         <TaskInput @name-task="value => nameTask = value"
-                    :changeState="changeState"
                     :nameTaskInput="nameTask" 
                     :date="props.date"/>
         <div class="tasks_box">
             <TaskCard v-for="(item, i) of cards"
-                        :changeState="changeState" 
-                        :key="item['id']" 
+                        :key="item['id']"
+                        :id="item.id" 
                         :date="props.date"
                         :number="i + 1"
                         :name="item['taskName']"
                         />
-
         </div>
     </div>
 </template>
@@ -39,8 +37,7 @@ const changeState = () => cards.value = LS.getTasks(props.date);
 <style scoped lang="scss">
     .task-canvas {
         min-width: 270px;
-        padding: 10px;
-        margin-left: 10px;
-        
+        padding: 5px;
+        margin-left: 5px;
     }
 </style>

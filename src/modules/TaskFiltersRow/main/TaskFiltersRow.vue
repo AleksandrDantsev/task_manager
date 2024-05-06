d90b83ecee4cbcee3149e5c62c5c7a789621b445
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-06 15:02:23 +0300

    add filter line

---
+++ src/modules/TaskFiltersRow/main/TaskFiltersRow.vue
@@ -0,0 +1,35 @@
+<script setup>
+import SwitcherDate from '../components/SwitcherDate.vue';
+
+const props = defineProps({
+    changeShiftDate: Function,
+    currentDate: String,
+})
+
+</script>
+
+<template>
+    <div class="filters">
+        <div class="wrapper">
+            <SwitcherDate @date-minus="props.changeShiftDate(true)"
+                          @date-plus="props.changeShiftDate(false)"
+                          :currentDate="props.currentDate"
+                />
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .filters {
+        display: flex;
+        justify-content: end;
+        width: 100%;
+        min-height: 40px;
+        padding: 0 18px;
+    }
+    .wrapper {
+        position: sticky;
+        left: 0;
+        width: 100vw;
+    }
+</style>

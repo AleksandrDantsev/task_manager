15d1969c91eb41859c0e72157887e474e594720d
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-06 14:57:10 +0300

    add card conteiner

---
+++ src/hoc/card_drop.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="card-drop">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .card-drop {
+        position: absolute;
+        top: 35px;
+        border-radius: 10px;
+        background-color: #fff;
+        box-shadow: 0 0 5px 0 #d4d4d4d8;
+    }
+</style>

f2237da2cd217d6cfbd923f29d833df67d0a0368
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-06 14:54:24 +0300

    add side bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
-
+<script setup>
+import { SideBar } from "./modules/SideBar/index";
+</script>
 <template>
     <div class="wrapper">
-      <header class="header">wer</header>
-      <router-view></router-view>
+        <SideBar />
+        <div class="main-window">
+            <header class="header">
+                
+            </header>
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-
+    .wrapper{
+        display: flex;
+        height: 100%;
+    }
+    .main-window {
+        flex-grow: 1;
+        height: 100%;
+    }
+    .header {
+        width: 100%;
+        height: 60px;
+        border-bottom: solid 1px #e6e6e6;
+    }
 </style>

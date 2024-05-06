6cc8a535270076dd98b166e7c1974ea4213c5f5e
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-06 14:59:29 +0300

    move in a separate component

---
+++ src/modules/TaskCanvas/components/MoveJoystick/MoveJoystick.vue
@@ -0,0 +1,51 @@
+<script setup>
+import joystick_arrow from "../../../../UI/joystick-arrow.vue";
+import { useStore } from "vuex";
+
+const props = defineProps({
+    date: String,
+    id: Number,
+})
+
+const store = useStore();
+// date, id, position
+const moveTaskIntoSibling = (direction) => {
+    store.commit("moveTask", [props.date, props.id, direction])
+}
+
+</script>
+
+<template>
+    <div class="task_card__move-joystick">
+        <div class="joystick-left" @click="() => moveTaskIntoSibling('left')">
+            <joystick_arrow :classname="'left_move'"/>
+        </div>
+        <div class="joystick-top-bottom">
+            <joystick_arrow :classname="'top_move'" @click="() => moveTaskIntoSibling('up')"/>
+            <joystick_arrow :classname="'bottom_move'" @click="() => moveTaskIntoSibling('down')"/>
+        </div>
+        <div class="joystick-right">
+            <joystick_arrow :classname="'right_move'" @click="() => moveTaskIntoSibling('right')"/>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .task_card__move-joystick {
+        position: absolute;
+        z-index: 10;
+        right: 15px;
+        top: 8px;
+        display: flex;
+        visibility: hidden;
+        opacity: 0;
+        transition-duration: 300ms;
+    }
+
+    .joystick-top-bottom {
+        display: flex;
+        flex-direction: column;
+    }
+
+
+</style>

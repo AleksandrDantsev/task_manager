b8c48c8a869f999078e85a6db31e2c5364a0988b
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-06 14:56:45 +0300

    add ability to get date with number

--- src/helpers/getReadableDate.js
+++ src/helpers/getReadableDate.js
@@ -1,4 +1,4 @@
-export const getReadableDate = (shiftBackDay = 0) => {
+export const getReadableDate = (shiftBackDay = 0, customDay=null) => {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     
@@ -8,7 +8,7 @@ export const getReadableDate = (shiftBackDay = 0) => {
     const day = date.getDate();
     const year = date.getYear();
 
-    const rightDate = new Date(year, mounth, day - shiftBackDay);
+    const rightDate = new Date(year, mounth, customDay === null ? day - shiftBackDay : customDay);
 
     const dayOfWeek = daysOfWeek[rightDate.getDay()];
     const dayOfMonth = rightDate.getDate();

d5b91f02cad5d108b532184897acf0922a52ef2d
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-06 14:57:26 +0300

    add pop-up window

---
+++ src/hoc/shadow.vue
@@ -0,0 +1,54 @@
+<script setup>
+const props = defineProps({
+    isOpenWindow: Boolean,
+});
+</script>
+
+<template>
+    <div @click.stop="$emit('close-window', $event.target.matches('.shadow-window'))" 
+         class="shadow-window" :class="{'open': isOpenWindow, 'close': props.isOpenWindow === false}">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .shadow-window{
+        background-color: #1b1b1b33;
+        position: fixed;
+        z-index: 900;
+        top: -20px;
+        left: 0;
+        right: 0;
+        bottom: -20px;
+        visibility: hidden;
+        opacity: 0;
+        transform: translate3d(0, 15px, 0);
+    }
+    .open {
+        animation: showAnim 400ms ease forwards;
+    }
+    .close {
+        animation: hideAnim 400ms ease forwards;
+    }
+
+    @keyframes showAnim {  
+        to {
+            visibility: visible;
+            opacity: 1;
+            transform: initial;
+        } 
+    }
+
+    @keyframes hideAnim {
+        // from {
+        //     visibility: visible;
+        //     opacity: 1;
+        //     transform: initial;
+        // } 
+        to {
+            visibility: hidden;
+            opacity: 0;
+            transform: translate3d(0, 15px, 0);
+        }   
+    }
+</style>

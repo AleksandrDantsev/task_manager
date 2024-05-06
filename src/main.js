82ab0cec3d7a2c4b0ecd98ecc65c27c0afce22f7
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-06 14:58:33 +0300

    add reducer

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
 import router from './router/router';
+import infoCardStore from './store/infoCardStore/infoCardStore';
 
 
   
 const app = createApp(App);
 app.use(router);
+app.use(infoCardStore);
 app.mount('#app');
+
+//  #e6e6e6  border
+//  #f5f5f5d8  backgrounв-color task-panel
+//  #b3b3b3  svg
+//  #d4d4d4d8 shadow
+//  #888686 gray-text

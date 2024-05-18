e53cc6fde40ab21fd2f7f74754f2296d4eb9b913
Aleksandr Dantsev <ivan_sidorov_99@internet.ru>
2024-05-18 14:18:40 +0300

    add pages

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,16 @@
-import TaskPanel from "../pages/TaskPanel/TaskPanel.vue"
-import { createRouter, createWebHistory } from 'vue-router';
+import TaskPanel from "../pages/TaskPanel/TaskPanel.vue";
+import MyTasks from "../pages/MyTasks/MyTasks.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-const routes  = [
-  { path: '/', name: 'TaskPanel', component: TaskPanel },
-  // { path: '/about', component: 'rtrty' }
+const routes = [
+    { path: "/", name: "TaskPanel", component: TaskPanel },
+    { path: "/my-tasks", component: MyTasks },
+    // { path: "/projects", component: "rtrty3333" },
 ];
 
-
 const router = createRouter({
-  history: createWebHistory(),
-  routes, 
+    history: createWebHistory(),
+    routes,
 });
 
 export default router;
